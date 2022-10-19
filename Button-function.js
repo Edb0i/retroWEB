051080fe235a005f1bfa994bbef734aae30fb583
Edb0i <111795233+Edb0i@users.noreply.github.com>
2022-10-19 11:53:38 -0500

    trial1
    added files to new repository

---
+++ Button-function.js
@@ -0,0 +1,50 @@
+
+$(".container2, .container3, .container4, .container5, .container6, .container7").hide();
+
+$(".Arin-button").click(function(){
+    $(".container2").fadeIn(2000);
+});
+
+$(".container2").click(function(){
+    $(this).fadeOut(2000);
+});
+
+$(".Dan-button").click(function(){
+    $(".container3").fadeIn(2000);
+});
+
+$(".container3").click(function(){
+    $(this).fadeOut(2000);
+});
+
+$(".Past-button").click(function(){
+    $(".container4").fadeIn(2000);
+});
+
+$(".container4").click(function(){
+    $(this).fadeOut(2000);
+});
+
+$(".Now-button").click(function(){
+    $(".container5").fadeIn(2000);
+});
+
+$(".container5").click(function(){
+    $(this).fadeOut(2000);
+});
+
+$(".Favorite-game-played-button").click(function(){
+    $(".container6").fadeIn(2000);
+});
+
+$(".container6").click(function(){
+    $(this).fadeOut(2000);
+});
+
+$(".Favorite-episode-button").click(function(){
+    $(".container7").fadeIn(2000);
+});
+
+$(".container7").click(function(){
+    $(this).fadeOut(2000);
+});
